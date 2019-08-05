b2786abb5913b37d28c50e68cc7453fa845d6f9d
Chanmi-Kim <chanmi_dev@naver.com>
2019-08-05 18:24:25 +0900

    add file basic.py
    - IDLE에서 작업한 내용입니다.

---
+++ basic.py
@@ -0,0 +1,103 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 10 + 20
+30
+>>> 10
+10
+>>> 10 + 20
+30
+>>> 10
+10
+>>> 10
+10
+>>> 3.14
+3.14
+>>> type(10)
+<class 'int'>
+>>> type(3.14)
+<class 'float'>
+>>> a = 10
+>>> a
+10
+>>> print(a)
+10
+>>> type(a)
+<class 'int'>
+>>> a = 3.14
+>>> a
+3.14
+>>> type(a)
+<class 'float'>
+>>> a = 10
+>>> a = '홍길동'
+>>> a
+'홍길동'
+>>> type(a)
+<class 'str'>
+>>> b = True
+>>> type(b)
+<class 'bool'>
+>>> b = true
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    b = true
+NameError: name 'true' is not defined
+>>> b = false
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    b = false
+NameError: name 'false' is not defined
+>>> b= False
+>>> 10 + 20
+30
+>>> a = 10
+>>> b = 20
+>>> a + b
+30
+>>> a - b
+-10
+>>> a * b
+200
+>>> a / b
+0.5
+>>> a = 2
+>>> b = 3
+>>> a * b
+6
+>>> a ** b
+8
+>>> a ** 8
+256
+>>> a = 10
+>>> b = 3
+>>> a / b
+3.3333333333333335
+>>> a // b
+3
+>>> a % b
+1
+>>> str1 = '홍길동';
+>>> str1 = '홍길동'
+>>> str1
+'홍길동'
+>>> str2 = "홍길동"
+>>> str2
+'홍길동'
+>>> str1 = '홍길동 "하우스" 입니다'
+>>> str2 = "홍길동 '하우스' 입니다"
+>>> str1 = '홍길동 \'하우스\' 입니다'
+>>> str3 = '''홍길동'''
+>>> str3
+'홍길동'
+>>> type(str3)
+<class 'str'>
+>>> str4 = """홍길동"""
+>>> str4
+'홍길동'
+>>> str3 = '''홍길동
+입니다'''
+>>> str3
+'홍길동\n입니다'
+>>> str1 = '홍길동
+SyntaxError: EOL while scanning string literal
+>>> 
