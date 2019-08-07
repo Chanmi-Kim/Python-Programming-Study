2065da5cf1b774b3efaa1aee796111b50684bbc2
Chanmi Kim <chanmi_dev@naver.com>
2019-08-07 21:36:15 +0900

    add file ex07.py
    - ex07.py 파일 추가

---
+++ PythonTest/src/ex07.py
@@ -0,0 +1,15 @@
+# ex07.py
+
+# from mod1 import mysum
+# from mod1 import mysub
+# from mod1 import mysum, mysub
+from mod1 import * # import mod1
+
+a = 20
+b = 10
+
+result = mysum(a, b)
+print(result)
+
+result = mysub(a, b)
+print(result)
