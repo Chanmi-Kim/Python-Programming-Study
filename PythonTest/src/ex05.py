fd11d8e3426cff2fe2760890592c0a8457738c3f
Chanmi-Kim <chanmi_dev@naver.com>
2019-08-07 17:46:08 +0900

    add metadata
    - from eclipse

--- PythonTest/src/ex05.py
+++ PythonTest/src/ex05.py
@@ -1,4 +1,5 @@
 # ex05.py
+from chardet.latin1prober import OTH
 
 # 파이썬 클래스
 class Item:
@@ -47,6 +48,87 @@ size1.setSize(300, 400)
 print(size1.width)
 print(size1.height)
 
+
+class Computer:
+    cpu = 'i5' # public
+    __memory = '16gb' # private
+    
+com = Computer()
+# print(com.cpu, com.__memory)
+# AttributeError: 'Computer' object has no attribute 'memory'
+
+# 생성자
+class Cup:
+    # 멤버 변수
+    # size = 0
+    # color = 'white'
+    
+    # 생성자(__init__)
+#     def __init__(self):
+#         self.size = 0
+#         self.color = 'white'
+    
+    def __init__(self, size, color):
+        self.size = size
+        self.color = color
+
+cup1 = Cup(100, 'blue')
+print(cup1.size)
+print(cup1.color)
+
+
+
+# 클래스 상속
+class Parent:
+    name = '부모'
+    def hello(self):
+        print('안녕하세요.')
+
+class Child(Parent):
+    nick = '자식'
+    name = '자식이 또 추가'
+    def hi(self):
+        print('방가~')
+    def hello(self):
+        print("Hi~")
+
+child = Child()
+print(child.name)
+print(child.nick)
+child.hello()
+child.hi()
+
+
+
+
+# 연산자 오버로딩(***)
+# - 연산자도 메소드처럼 취급해서 오버로딩을 가능하게 만드는 기술
+# result = sum(10, 20) # sum(int, int)
+# result = sum(3.5, 2.5) # sum(float, float)
+# 
+# result = 10 + 20
+# result = 2.5 + 2.5
+
+class Note:
+    page = 0
+    color = ''
+    def __init__(self, page, color):
+        self.page = page
+        self.color = color
+    def __add__(self, other):
+        # print(self.page + other.page)
+        return Note(self.page + other.page, self.color)
+
+n1 = Note(100, 'white')
+n2 = Note(150, 'black')
+
+# Note + Note = Note
+# print(n1 + n2)
+n3 = n1 + n2 # + n4 + n5
+print(n3.page)
+print(n3.color)
+
+#n3 = bindNote(bindNote(bindNote(n1, n2), n4), n5)
 
 
 
