926c4727e0b3cb36d922495792dbbc62bb10d66f
Chanmi-Kim <chanmi_dev@naver.com>
2019-08-09 17:34:00 +0900

    modif file test.py
    - test.py 파일 수정

--- PythonTest/src/test.py
+++ PythonTest/src/test.py
@@ -3,3 +3,12 @@ import cx_Oracle as oci
 conn = oci.connect('hr/java1234@localhost:1521/xe')
 
 print(conn.version)
+
+cursor = conn.cursor()
+
+sql = 'insert into tblMemo values (2, :name, :memo, default, :category)'
+result = cursor.execute(sql, name='아무개', memo='테스트입니다.', category='할일')
+conn.commit()
+
+print(result)
+
