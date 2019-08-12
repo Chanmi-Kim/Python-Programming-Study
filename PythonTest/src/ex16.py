da5732fa247c34d75acefe7283ee994a83af0116
Chanmi-Kim <chanmi_dev@naver.com>
2019-08-12 13:47:17 +0900

    modify file ex16.py
    - ex16.py 파일 수정

--- PythonTest/src/ex16.py
+++ PythonTest/src/ex16.py
@@ -3,33 +3,39 @@
 from myrequest import get
 from bs4 import BeautifulSoup
 import urllib
+import urllib3
 import os
 
 site = 'https://m.comic.naver.com/index.nhn'
 
-sample = 'http://t1.daumcdn.net/webtoon_episode/m/709baef8f46143399fb1fcacc4c5073b'
+#sample = 'http://t1.daumcdn.net/webtoon_episode/m/709baef8f46143399fb1fcacc4c5073b'
+sample = 'http://ttcfd.hanatour.com/@cms_300/2014062337/gjdol5/IFC 55층 전망대_1.jpg'
 
 # ('https://image-comic.pstatic.net/mobilewebimg/641253/246', '6c92e168f20860f89af021f1d8b35140_001.jpg')
 
-# print(os.path.split(sample))
+print(os.path.split(sample))
 # sampleName = os.path.split(sample)[1]
 
 # print(sampleName)
 
 # 파일 저장(다운로드)
-# urllib.request.urlretrieve(sample, 'webtoon/test.png')
+urllib.request.urlretrieve(urllib.parse.quote(sample.encode('utf8'), '/:'), 'webtoon/test.png')
 # print('저장 완료')
 
 
-
+'''
 url = 'http://211.63.89.31:8088/spring/list.action'
 html = get(url)
 soup = BeautifulSoup(html, 'html.parser')
+'''
+
 
 # selector = 'head > meta'
 # meta = soup.select(selector)
 # print(meta)
 
+
+'''
 selector = '#list > div > img'
 images = soup.select(selector)
 
@@ -49,7 +55,7 @@ print('완료')
 
 
 
-
+'''
 
 
 
